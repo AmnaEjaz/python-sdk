c7b313cfcd1c50a25c4243022113cb8821108acc
uzair-folio3 <unadeem@folio3.com>
2020-09-02 12:44:39 +0500

    semver new validations added with test cases and comment addressed

--- optimizely/helpers/condition.py
+++ optimizely/helpers/condition.py
@@ -141,34 +141,6 @@ class CustomAttributeConditionEvaluator(object):
         """
         return VersionType.HAS_WHITE_SPACE in version
 
-    def is_build_limit_exceeded(self, version):
-        """ Method to check if the given version contains two + (1.0.0+1+2)
-
-        Args:
-          version: Given version in string.
-
-        Returns:
-          Boolean:
-            - True if the given version does contain whitespace
-            - False if it doesn't
-        """
-
-        return len(version.split(VersionType.IS_BUILD)) > 2
-
-    def is_release_limit_exceeded(self, version):
-        """ Method to check if the given version contains two + (1.0.0+1+2)
-
-        Args:
-          version: Given version in string.
-
-        Returns:
-          Boolean:
-            - True if the given version does contain whitespace
-            - False if it doesn't
-        """
-
-        return len(version.split(VersionType.IS_PRE_RELEASE)) > 3
-
     def compare_user_version_with_target_version(self, target_version, user_version):
         """ Method to compare user version with target version.
 
@@ -217,29 +189,39 @@ class CustomAttributeConditionEvaluator(object):
                 elif user_version_part < target_version_part:
                     return -1
 
+        # check if user has pre-release or build and target hasn't
+        if (user_version_parts[1] or user_version_parts[2]) and (
+                not target_version_parts[1] and not target_version_parts[2]):
+            return -1
+
+        elif (not user_version_parts[1] and not user_version_parts[2]) and (
+                target_version_parts[1] or target_version_parts[2]):
+            return 1
+
+        # checking build
+        if len(user_version_parts[2]) < len(target_version_parts[2]):
+            return -1
+        user_pre_release_parts_len = len(user_version_parts[2])
+
+        for (idx, _) in enumerate(target_version_parts[2]):
+            if user_pre_release_parts_len > idx:
+                if user_version_parts[2][idx] < target_version_parts[2][idx]:
+                    return -1
+                elif user_version_parts[2][idx] > target_version_parts[2][idx]:
+                    return 1
+
         # checking pre-release
-            if len(user_version_parts[1]) < len(target_version_parts[1]):
-                return -1
+        if len(user_version_parts[1]) < len(target_version_parts[1]):
+            return -1
 
-            user_pre_release_parts_len = len(user_version_parts[1])
+        user_pre_release_parts_len = len(user_version_parts[1])
 
-            for (idx, _) in enumerate(target_version_parts[1]):
-                if user_pre_release_parts_len > idx:
-                    if user_version_parts[1][idx] < target_version_parts[1][idx]:
-                        return -1
-                    elif user_version_parts[1][idx] > target_version_parts[1][idx]:
-                        return 1
-
-            if len(user_version_parts[2]) < len(target_version_parts[2]):
-                return -1
-            user_pre_release_parts_len = len(user_version_parts[2])
-
-            for (idx, _) in enumerate(target_version_parts[2]):
-                if user_pre_release_parts_len > idx:
-                    if user_version_parts[2][idx] < target_version_parts[2][idx]:
-                        return -1
-                    elif user_version_parts[2][idx] > target_version_parts[2][idx]:
-                        return 1
+        for (idx, _) in enumerate(target_version_parts[1]):
+            if user_pre_release_parts_len > idx:
+                if user_version_parts[1][idx] < target_version_parts[1][idx]:
+                    return -1
+                elif user_version_parts[1][idx] > target_version_parts[1][idx]:
+                    return 1
         return 0
 
     def exact_evaluator(self, index):
@@ -667,8 +649,7 @@ class CustomAttributeConditionEvaluator(object):
         builds = []
 
         # check that version shouldn't have white space, more than 1 builds and more than 2 pre-releases
-        if self.has_white_space(version) or self.is_release_limit_exceeded(version) or \
-                self.is_build_limit_exceeded(version):
+        if self.has_white_space(version):
             self.logger.warning(Errors.INVALID_ATTRIBUTE_FORMAT)
             return None
 
@@ -701,20 +682,9 @@ class CustomAttributeConditionEvaluator(object):
         elif self.is_build(version):
             target_parts = version.split(VersionType.IS_BUILD)
 
-            # break into prefix and suffix
+            # break into prefix and suffix (in tis case builds)
             target_prefix = str(target_parts[0])
-            target_suffix = ''.join(target_parts[1:])
-
-            if self.is_pre_release(target_suffix):
-                target_parts = target_suffix.split(VersionType.IS_PRE_RELEASE)
-
-                # assign first part into build as with was first split using VersionType.IS_BUILD and
-                # second part into pre-release
-                builds.append(target_parts[0])
-                pre_releases.append(target_parts[1])
-            else:
-                # if there is no release then assign whole part into build
-                builds.extend(target_parts[1:])
+            builds.extend(target_parts[1:])
 
         # check dot counts in target_prefix
         dot_count = target_prefix.count(".")
--- tests/helpers_tests/test_condition.py
+++ tests/helpers_tests/test_condition.py
@@ -236,7 +236,6 @@ class CustomAttributeConditionEvaluatorTest(base.BaseTest):
         semver_greater_than_2_0_condition_list = [['Android', "2.0", 'custom_attribute', 'semver_gt']]
         versions = [
             ('2.0.1', '2.0.1'),
-            ('2.0.0', '2.0.0'),
             ('2.9', '2.9.1'),
             ('2.9.9', '2.9.9'),
             ('2.9.9-beta', '2.9.9-beta'),
@@ -245,10 +244,8 @@ class CustomAttributeConditionEvaluatorTest(base.BaseTest):
             ('2', '2.12'),
             ('2', '2.785.13'),
             ('2', '2.785.13'),
-            ('2.9.9', '2.9.9-beta'),
             ('2.9.9-beta', '2.9.9-beta+beta'),
             ('2.9.9-beta', '2.9.9-beta-beta'),
-            ('2.9.9', '2.9.9+beta-beta'),
         ]
         for target_version, user_version in versions:
             evaluator = condition_helper.CustomAttributeConditionEvaluator(
@@ -268,9 +265,11 @@ class CustomAttributeConditionEvaluatorTest(base.BaseTest):
             ('2.0', '3.0.1'),
             ('2.0', '2.9.1'),
             ('2.9.0', '2.9.1'),
-            ('2.1.2', '2.1.3-beta'),
             ('2.1.2-beta', '2.1.2-release'),
             ('2.1.3-beta1', '2.1.3-beta2'),
+            ('2.9.9-beta', '2.9.9'),
+            ('2.9.9+beta', '2.9.9'),
+            ('3.7.0-prerelease+build', '3.7.0-prerelease+rc'),
             ('2.2.3-beta-beta1', '2.2.3-beta-beta2'),
             ('2.2.3-beta+beta1', '2.2.3-beta+beta2'),
             ('2.2.3+beta2-beta1', '2.2.3+beta3-beta2')
@@ -294,7 +293,9 @@ class CustomAttributeConditionEvaluatorTest(base.BaseTest):
             ('2.3.5', '2.3.1'),
             ('2.9.8', '2.9'),
             ('2.1.2-release', '2.1.2-beta'),
-            ('2.1.3-beta', '2.1.3'),
+            ('2.1.3', '2.1.3-beta'),
+            ('2.1.3', '2.1.3+beta'),
+            ('3.7.0-prerelease', '3.7.0+build3.7.0-prerelease+build'),
             ('2.1.3-beta-beta2', '2.1.3-beta'),
             ('2.1.3-beta1+beta3', '2.1.3-beta1+beta2')
         ]
@@ -312,8 +313,7 @@ class CustomAttributeConditionEvaluatorTest(base.BaseTest):
         semver_greater_than_2_0_condition_list = [['Android', "2.0", 'custom_attribute', 'semver_gt']]
         versions = [
             '-', '.', '..', '+', '+test', ' ', '2 .3. 0', '2.', '.2.2', '3.7.2.2', '3.x', ',', '+build-prerelease',
-            '2.1.3+1+2', '02.1.3+1', '2.1.3-1-2-3'
-        ]
+            '02.1.3+1', '2..2']
         target_version = '2.1.0'
 
         for user_version in versions:
